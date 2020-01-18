9414cccd77453fbee7afb9cdd50de9eb7ba053dc
Danil Lykov <lkv97dn@gmail.com>
2020-01-18 15:03:31 -0600

    some fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
 abbr -a gl "git log --decorate --color --graph --all  --pretty=format:'%C(yellow)%h %Cred%ad %Creset%s%x09%Cblue%an%Cgreen%d ' --date=relative"
 
+alias vim=nvim
+alias xclip="xclip -selection 'clipboard'"
+
