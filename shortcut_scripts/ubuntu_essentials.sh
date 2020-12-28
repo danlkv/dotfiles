2e8d3f692df7aa52facc7b66e5426c79686df6db
Danil Lykov <lkv97dn@gmail.com>
2020-12-28 08:36:06 -0600

    add shortcut scripts for ubuntu

---
+++ shortcut_scripts/ubuntu_essentials.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y neovim tmux tree unzip
+sudo apt install -y python3 python3-pip python3-numpy python3-matplotlib python3-scipy
+sudo ln -srf $(which python3) /usr/bin/python
+sudo ln -srf $(which pip3) /usr/bin/pip
+
+sudo pip install jupyter pytest
---
+++ shortcut_scripts/ubuntu_ml_torch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y python3-pandas
+
+sudo pip install torch torchvision scikit-learn
