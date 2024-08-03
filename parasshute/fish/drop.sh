8293a3a53c42f5cd55db2da9f52d083869643b03
Dan <lkv97dn@gmail.com>
2024-08-02 23:36:42 -0500

    fish shell: use 3.7.1

--- parasshute/fish/drop.sh
+++ parasshute/fish/drop.sh
@@ -20,14 +20,36 @@ if [ -z "$INSTALL_PREFIX" ]; then
     exit 1
 fi
 
-echo "Installing source into SOURCE_DIR: $SOURCE_DIR"
+# check if make and cmake are installed
+
+if ! command -v make > /dev/null; then
+    echo "make is not installed"
+    exit 1
+fi
+
+if ! command -v cmake > /dev/null; then
+    echo "cmake is not installed"
+    exit 1
+fi
+
+echo "Downloading fish source into SOURCE_DIR: $SOURCE_DIR"
 echo "Prefix is INSTALL_PREFIX: $INSTALL_PREFIX"
 
 # if `fish` does not exist, install it
 
 mkdir -p $SOURCE_DIR
 git clone https://github.com/fish-shell/fish-shell.git $SOURCE_DIR/fish-shell
+#
+# Note: fish will port to rust in future.
+#   The reason is that developers want to be trendy, and are tired of C++.
+#   For me, rust is a good but heavy language (.5GB on alpine), with llvm on top.
+#   The build time is longer.
+#   c++ and cmake are available on most systems.
+#
+#   Thus, cehckout to 3.7.1
+#
 pushd $SOURCE_DIR/fish-shell
+git checkout 3.7.1
 cmake . -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 make -j; make install
 
