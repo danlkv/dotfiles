b23730c0775bb283e087f009359f725f04a7113a
Danylo Lykov <dlykov@nvidia.com>
2024-09-26 21:35:11 -0500

    fzf fish

---
+++ parasshute/fish-fzf/drop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# DEPEND: fisher
+./drop.sh fisher || exit 1
+export PATH=$INSTALL_PREFIX/bin:$PATH
+version=$(fish -c "fisher list fzf")
+if [ -n "$version" ]; then
+    echo "FZF is already installed"
+    exit 0
+fi
+cmd="fisher install /patrickf1/fzf.fish"
+fish -c "$cmd" || exit 1
