3a79763ba48177b5c9dee83255e8a728c5028571
Dan <lkv97dn@gmail.com>
2025-01-27 18:11:51 -0600

    fish movement bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ abbr -a glo "git log --graph --decorate --topo-order --date=format-local:'%Y-%m-
 abbr -a gl "git log --decorate --color --graph --all  --pretty=format:'%C(yellow)%h %Cred%ad %Creset%s%x09%Cblue%an%Cgreen%d ' --date=relative"
 git config --global alias.lg "log --graph --decorate -30 --topo-order --date=format-local:'%Y-%m-%d %H:%M:%S' --pretty=format:'%C(cyan)%h%Creset %C(black bold)%ad %C(dim yellow)(%an) %Creset%C(auto)%d %s'"
 
+# -- Vim keybindings
+fish_vi_key_bindings
+
 alias vim=nvim
 alias tmux="tmux -2"
 alias clip="xclip -selection 'clipboard'"
@@ -16,31 +19,38 @@ bind -s  -M insert \cs end-of-line
 bind -s  -M default \ce end-of-line
 bind -s  -M insert \ce end-of-line
 bind \cs end-of-line
-# -- C-a acts as "Go to first character" (emacs,bash)
+# -- C-a - go to line start (emacs,bash)
 bind -s  -M default \cA beginning-of-line
 bind -s  -M insert \cA beginning-of-line
 
-# -- Vim keybindings
-fish_vi_key_bindings
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
 
-    # -- Check if fzf is installed
-    if functions -q fzf_configure_bindings
-        # -- Configure fzf
-        export FZF_DEFAULT_OPTS='--height 40%'
+  # -- Check if fzf is installed
+  if functions -q fzf_configure_bindings
+    # -- Configure fzf
+    export FZF_DEFAULT_OPTS='--height 40%'
 
-        bind -M insert \cP _fzf_search_history
-        bind -M default \cP _fzf_search_history
-    else
-        bind -M insert \cP up-or-search
-        bind -M default \cP up-or-search
-    end
+    bind -M insert \cP _fzf_search_history
+    bind -M default \cP _fzf_search_history
+  else
+    bind -M insert \cP up-or-search
+    bind -M default \cP up-or-search
+  end
 
-    bind -M insert \cE end-of-line
-    bind -s  -M default \cA beginning-of-line
-    bind -s  -M insert \cA beginning-of-line
+  bind -M insert \cE end-of-line
+  bind -s  -M default \cA beginning-of-line
+  bind -s  -M insert \cA beginning-of-line
+
+  # -- Movements
+  # -- -m ensures that we don't exit the mode (without it we exit into default)
+  # ---- C-f - "Go to next word" (emacs,bash)
+  bind -s  -M default -m default \ef forward-word
+  bind -s  -M insert -m insert \ef forward-word 
+  # ---- C-b - "Go to previous word" (emacs,bash)
+  bind -s  -M default -m default \eb backward-word
+  bind -s  -M insert -m insert \eb backward-word 
 end
 
 #source $HOME/.config/danlkv_platforms/$(hostname)
